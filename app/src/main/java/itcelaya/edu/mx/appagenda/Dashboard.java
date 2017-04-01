15ade6b0e069ffca6890e3c8608a105f88f7a8bd
EmmanuelNOVA <env56801@udelasalle.edu.mx>
2017-04-01 12:48:47 -0600

    Splash screen

---
+++ app/src/main/java/itcelaya/edu/mx/appagenda/Dashboard.java
@@ -0,0 +1,17 @@
+package itcelaya.edu.mx.appagenda;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by emmanuel-nova on 01/04/17.
+ */
+
+public class Dashboard extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
